84717cda6cb64aefa81b9999752c20fdf9278fa0
akadu GitHub <tambesaurabh14@gmail.com>
2025-08-04 14:13:29 +0530

    first commit

--- src/pages/H.js
+++ src/pages/H.js
@@ -4,7 +4,6 @@ export default function TodoList(props) {
   return (
     <li className="list-item">
       <div className="list-item-content">
-        hiiii 
         <h1>Create Your List</h1>
         </div>
     </li>
